d9ab0f5e8b5e66c36437a5b6633316a96817f9e3
Jojo <jordangrilly@gmail.com>
2023-03-16 22:17:58 +0100

    Update : Add Sequence

---
+++ Assets/Script/BehaviourTree/Sequence.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BehaviorTree
+{
+    public class Sequence : Node
+    {
+        public Sequence() : base() { }
+        public Sequence(List<Node> children) : base(children) { }
+        public override NodeState Evaluate()
+        {
+            bool anyChildIsRunning = false;
+            
+            foreach(Node node in children)
+            {
+                switch(node.Evaluate())
+                {
+                    case NodeState.FAILURE:
+                        state = NodeState.FAILURE;
+                        return state;
+                    case NodeState.SUCCESS:
+                        state = NodeState.SUCCESS;
+                        return state;
+                    case NodeState.RUNNING:
+                        state = NodeState.RUNNING;
+                        return state;
+                }
+            }
+
+            state = anyChildIsRunning ? NodeState.RUNNING : NodeState.SUCCESS;
+            return state;
+        }
+    }
+}
