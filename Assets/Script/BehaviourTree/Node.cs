b423349a67716d712366eca4330c56c2a82269b5
Jojo <jordangrilly@gmail.com>
2023-03-16 21:52:16 +0100

    Update : Complete Node system for behavior tree

--- Assets/Script/BehaviourTree/Node.cs
+++ Assets/Script/BehaviourTree/Node.cs
@@ -12,10 +12,12 @@ namespace BehaviorTree
     } 
     public class Node
     {
-        protected NodeState state;
-
         public Node parent;
+
+        protected NodeState state;
         protected List<Node> children = new List<Node>();
+
+        private Dictionary<string, object> data = new Dictionary<string, object>();
 
         public Node()
         {
@@ -37,6 +39,54 @@ namespace BehaviorTree
         }
 
         public virtual NodeState Evaluate => NodeState.FAILURE; 
+
+        public void SetData(string key, object value)
+        {
+            data[key] = value;
+        }
+
+        public object GetData(string key)
+        {
+            object value = null;
+            if (data.TryGetValue(key, out value))
+            {
+                return value;
+            }
+
+            Node node = parent;
+            
+            while(node != null)
+            {
+                value = node.GetData(key);
+                if(value != null)
+                {
+                    return value;
+                }
+
+                node = node.parent;
+            }
+
+            return null;
+        }
+
+        public bool ClearData(string key)
+        {
+            if(data.ContainsKey(key))
+            {
+                data.Remove(key);
+                return true;
+            }
+
+            Node node = parent;
+            while(node != null)
+            {
+                bool cleared = node.ClearData(key);
+                if (cleared)
+                    return true;
+
+                node = node.parent;
+            }
+            return false;
+        }
     }
 }
-
