fb2e0601ec54074b123ea9492c7b9d8e80f1fea3
Jojo <jordangrilly@gmail.com>
2023-03-16 21:57:50 +0100

    Update : Add comment to Node

--- Assets/Script/BehaviourTree/Node.cs
+++ Assets/Script/BehaviourTree/Node.cs
@@ -4,61 +4,82 @@ using UnityEngine;
 
 namespace BehaviorTree
 {
+    // Define the possible states for a node
     public enum NodeState
     {
         RUNNING,
         SUCCESS,
         FAILURE,
-    } 
+    }
+
+    // Define the base class for a behavior tree node
     public class Node
     {
+        // A reference to the parent node
         public Node parent;
 
+        // The current state of the node
         protected NodeState state;
+
+        // A list of child nodes
         protected List<Node> children = new List<Node>();
 
+        // A dictionary to store node-specific data
         private Dictionary<string, object> data = new Dictionary<string, object>();
 
+        // Default constructor
         public Node()
         {
             parent = null;
         }
 
+        // Constructor that takes a list of child nodes
         public Node(List<Node> children)
         {
-            foreach(Node child in children)
+            // Attach each child node to this parent node
+            foreach (Node child in children)
             {
                 _Attach(child);
             }
         }
 
+        // Attach a child node to this parent node
         private void _Attach(Node node)
         {
             node.parent = this;
             children.Add(node);
         }
 
-        public virtual NodeState Evaluate => NodeState.FAILURE; 
+        // The virtual Evaluate method, which can be overridden by child classes
+        // to define their specific behavior
+        public virtual NodeState Evaluate => NodeState.FAILURE;
 
+        // Set a key-value pair in the node's data dictionary
         public void SetData(string key, object value)
         {
             data[key] = value;
         }
 
+        // Get the value associated with a key in the node's data dictionary
         public object GetData(string key)
         {
             object value = null;
+
+            // Check if the key exists in this node's data dictionary
             if (data.TryGetValue(key, out value))
             {
                 return value;
             }
 
+            // If the key doesn't exist in this node's data dictionary, check the parent node
             Node node = parent;
-            
-            while(node != null)
+
+            while (node != null)
             {
                 value = node.GetData(key);
-                if(value != null)
+
+                // If the value is found in a parent node, return it
+                if (value != null)
                 {
                     return value;
                 }
@@ -66,19 +87,22 @@ namespace BehaviorTree
                 node = node.parent;
             }
 
+            // If the key isn't found in any node, return null
             return null;
         }
 
+        // Define a method to remove a key-value pair from the node's data dictionary
         public bool ClearData(string key)
         {
-            if(data.ContainsKey(key))
+            if (data.ContainsKey(key))
             {
                 data.Remove(key);
                 return true;
             }
 
+            // If the value isn't in this node's data dictionary, look in the parent node's data dictionary
             Node node = parent;
-            while(node != null)
+            while (node != null)
             {
                 bool cleared = node.ClearData(key);
                 if (cleared)
@@ -86,6 +110,8 @@ namespace BehaviorTree
 
                 node = node.parent;
             }
+
+            // If the key-value pair isn't found in any data dictionary, return false
             return false;
         }
     }
