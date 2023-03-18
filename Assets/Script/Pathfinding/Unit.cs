2da24c68b28a50ccef92eb40e9dd331419a45f4a
Jojo <jordangrilly@gmail.com>
2023-03-18 23:15:45 +0100

    Update : Make the path smoother

--- Assets/Script/Pathfinding/Unit.cs
+++ Assets/Script/Pathfinding/Unit.cs
@@ -6,20 +6,23 @@ public class Unit : MonoBehaviour
 {
     public Transform target;
     public float speed = 20;
-    Vector3[] path;
-    int targetIndex;
+    public float turnDist = 5;
+    //Vector3[] path;
+    //int targetIndex;
+
+    Path path;
 
     void Start()
     {
         PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
     }
 
-    public void OnPathFound(Vector3[] newPath, bool pathSuccessful)
+    public void OnPathFound(Vector3[] waypoints, bool pathSuccessful)
     {
         if (pathSuccessful)
         {
-            path = newPath;
-            targetIndex = 0;
+            path = new Path(waypoints, transform.position, turnDist);
+            //targetIndex = 0;
             StopCoroutine("FollowPath");
             StartCoroutine("FollowPath");
         }
@@ -27,24 +30,24 @@ public class Unit : MonoBehaviour
 
     IEnumerator FollowPath()
     {
-        Vector3 currentWaypoint = path[0];
+        //Vector3 currentWaypoint = path[0];
 
         while (true)
         {
-            if (transform.position == currentWaypoint)
-            {
-                targetIndex++;
-                if (targetIndex >= path.Length)
-                {
-                    targetIndex = 0;
-                    path = new Vector3[0];
-                    yield break;
-                }
+            //if (transform.position == currentWaypoint)
+            //{
+            //    targetIndex++;
+            //    if (targetIndex >= path.Length)
+            //    {
+            //        targetIndex = 0;
+            //        path = new Vector3[0];
+            //        yield break;
+            //    }
 
-                currentWaypoint = path[targetIndex];
-            }
+            //    currentWaypoint = path[targetIndex];
+            //}
 
-            transform.position = Vector3.MoveTowards(transform.position, currentWaypoint, speed * Time.deltaTime);
+            //transform.position = Vector3.MoveTowards(transform.position, currentWaypoint, speed * Time.deltaTime);
             yield return null;
         }
     }
@@ -53,20 +56,22 @@ public class Unit : MonoBehaviour
     {
         if (path != null)
         {
-            for (int i = targetIndex; i < path.Length; i++)
-            {
-                Gizmos.color = Color.black;
-                Gizmos.DrawCube(path[i], Vector3.one);
+            path.DrawWithGizmos();
+            
+            //for (int i = targetIndex; i < path.Length; i++)
+            //{
+            //    Gizmos.color = Color.black;
+            //    Gizmos.DrawCube(path[i], Vector3.one);
 
-                if (i == targetIndex)
-                {
-                    Gizmos.DrawLine(transform.position, path[i]);
-                }
-                else
-                {
-                    Gizmos.DrawLine(path[i - 1], path[i]);
-                }
-            }
+            //    if (i == targetIndex)
+            //    {
+            //        Gizmos.DrawLine(transform.position, path[i]);
+            //    }
+            //    else
+            //    {
+            //        Gizmos.DrawLine(path[i - 1], path[i]);
+            //    }
+            //}
         }
     }
 }
